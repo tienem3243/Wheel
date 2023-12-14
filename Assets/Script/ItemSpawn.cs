d3b3265facc1509bfcbd2ebb66d16ec62ee7ff17
tienem3243 <63142587+tienem3243@users.noreply.github.com>
2023-12-14 13:22:32 +0700

    Init

---
+++ Assets/Script/ItemSpawn.cs
@@ -0,0 +1,25 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSpawn : MonoBehaviour
+{
+     public ItemData data;
+    [SerializeField] GameObject prefabImage;
+    [SerializeField] Transform root;
+    public List<GameObject> items = new List<GameObject>();
+    public void Spawn()
+    {
+        foreach (var item in data.itemList)
+        {
+            Debug.Log("----> Spawn");
+            GameObject obj=Instantiate(prefabImage,root);
+            var image=obj.GetComponent<Image>();
+            image.sprite=item.sprite;
+            obj.name = item.id.ToString();
+            items.Add(image.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/LuckySwing.cs
@@ -0,0 +1,137 @@
+﻿using DG.Tweening;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LuckySwing : MonoBehaviour
+{
+
+    public ItemSpawn spawn; // Number of objects to spawn
+    public float circleRadius = 3f; // Radius of the circle
+    public int numberOfSpins;
+    public float rotationDuration;
+    public float goal;
+    private bool isSpinning;
+    public UnityEvent onSpinEnd;
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, circleRadius);
+        DrawLineToTargetAngle();
+        Gizmos.color = Color.red;
+        for (int i = 0; i < spawn.items.Count; i++)
+        {
+            float angle = i * Mathf.PI * 2 / spawn.items.Count;
+            float x = Mathf.Cos(angle) * circleRadius;
+            float y = Mathf.Sin(angle) * circleRadius;
+            Vector3 spawnPosition = new Vector3(x, y, 0f);
+         
+            Gizmos.DrawWireSphere(spawnPosition+transform.position,50);
+        }
+    }
+    void Start()
+    {
+        spawn.Spawn();
+        if (spawn.items.Count > 0)
+            SpawnUIObjects();
+        
+    }
+
+    Item GetRandomWeightedItem(Item[] itemList)
+    {
+        // Calculate total weight
+        float totalWeight = 0f;
+        foreach (var item in itemList)
+        {
+            totalWeight += item.weight;
+        }
+
+        // Generate a random value within the total weight range
+        float randomValue = UnityEngine.Random.Range(0f, totalWeight);
+
+        // Iterate through the items to find the one corresponding to the random value
+        foreach (var item in itemList)
+        {
+            if (randomValue <= item.weight)
+            {
+                return item;
+            }
+            randomValue -= item.weight;
+        }
+
+        // This line should not be reached, but return null if there's an issue
+        return null;
+    }
+
+void DrawLineToTargetAngle()
+    {
+        // Vị trí của đối tượng
+        Vector3 objectPosition = transform.position;
+
+        // Góc đích đến được chuyển đổi thành vector hướng
+        Vector3 targetDirection = Quaternion.Euler(0,0, goal) * Vector3.up;
+
+        // Điểm kết thúc của đường là vị trí của đối tượng cộng với vector hướng đích đến
+        Vector3 lineEnd = objectPosition + targetDirection * 5000f; // Để dài hơn, bạn có thể điều chỉnh giá trị 5f
+
+        // Vẽ đường từ vị trí đối tượng đến điểm kết thúc
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(objectPosition, lineEnd);
+
+    }
+
+
+    void SpawnUIObjects()
+    {
+        for (int i = 0; i < spawn.items.Count; i++)
+        {
+            float angle = i * Mathf.PI * 2 / spawn.items.Count;
+            float x = Mathf.Cos(angle) * circleRadius;
+            float y = Mathf.Sin(angle) * circleRadius;
+
+            Vector3 spawnPosition = new Vector3(x, y, 0f)+transform.localPosition;
+            spawn.items[i].transform.localPosition=spawnPosition;
+
+        }
+    }
+
+   public void SpinObject(float targetAngle)
+    {
+        // Tính toán tổng số độ cần xoay dựa trên targetRotation
+        float totalRotation =transform.rotation.eulerAngles.z+ targetAngle + (360 * numberOfSpins);
+
+        isSpinning = true;
+        // Tạo tween để xoay đối tượng
+        transform.DORotate(new Vector3(0, 0, totalRotation), rotationDuration, RotateMode.FastBeyond360)
+            .SetEase(Ease.OutQuint)
+            .OnComplete(() =>
+            {
+                Debug.Log("Xoay xong!");
+            }).OnComplete(() =>
+            {
+                transform.eulerAngles = new Vector3(0, 0, totalRotation);
+                isSpinning = false;
+                onSpinEnd.Invoke();
+            });
+    }
+    public void SpinObject(Transform target)
+    {
+        var goalDir =  Quaternion.Euler(0, 0, goal) * Vector3.up;
+        Vector3 to = target.position - transform.position;
+        SpinObject(Vector3.Angle(goalDir, to));
+       Debug.Log(Vector3.Angle(goalDir, to));
+    }
+    public void Spin()
+    {
+        if (isSpinning) return;
+       Item item= GetRandomWeightedItem(spawn.data.itemList.ToArray());
+        Debug.Log(item.id + " was choice");
+        if (item != null)
+        {
+           GameObject target= spawn.items.Find(x => x.name == item.id.ToString());
+            SpinObject(target.transform);
+        }
+    }
+
+
+
+}
